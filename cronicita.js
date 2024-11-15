b862127a9b8a9ca132dfb3c9a9f2d0407a4b6dc4
achieti <a.chieti@aress.regione.puglia.it>
2024-11-15 10:29:58 +0100

    Prova centralizzazione modifiche cronicità
    Sto provando a creare un JS contenente le variabili che servono a creare i filtri personalizzati dei grafici delle cronicità.
    Così facendo la modifica in aggiornamento dell'anno andrebbe fatta solo in un file.

---
+++ cronicita.js
@@ -0,0 +1,16 @@
+var yearOptions = [
+  { val: "2019", checked: true },
+  { val: "2018", checked: false },
+  { val: "2017", checked: false },
+  { val: "2016", checked: false },
+  { val: "2015", checked: false },
+  { val: "2014", checked: false },
+  { val: "2013", checked: false },
+  { val: "2012", checked: false },
+  { val: "2011", checked: false },
+  { val: "2010", checked: false },
+  { val: "2009", checked: false },
+  { val: "2008", checked: false },
+  { val: "2007", checked: false },
+  { val: "2006", checked: false },
+];
