386e9af637a923dd70f76cfdf14f8bafdf7cab55
achieti <a.chieti@aress.regione.puglia.it>
2024-11-28 13:07:47 +0100

    Update cronicita-grafico1.js

--- cronicita-grafico1.js
+++ cronicita-grafico1.js
@@ -13,7 +13,14 @@ function legendPoints(pathology) {
             points['from4'] = points['to4'];
             break;
         case 'BPCO':
-            article = 'della ';
+            points['to1'] = 54.91;
+            points['from1'] = points['to1'];
+            points['to2'] = 60.91;
+            points['from2'] = points['to2'];
+            points['to3'] = 65.43;
+            points['from3'] = points['to3'];
+            points['to4'] = 72.44;
+            points['from4'] = points['to4'];
             break;
         case 'Ipertensione':
             points['to1'] = 397.35;
@@ -22,11 +29,18 @@ function legendPoints(pathology) {
             points['from2'] = points['to2'];
             points['to3'] = 428.68;
             points['from3'] = points['to3'];
-            points['to4'] = 442.89;
+            points['to4'] = 112.61;
             points['from4'] = points['to4'];
             break;
         default:
-            article = ' del ';
+            points['to1'] = 102.02;
+            points['from1'] = points['to1'];
+            points['to2'] = 109.77;
+            points['from2'] = points['to2'];
+            points['to3'] = 118.33;
+            points['from3'] = points['to3'];
+            points['to4'] = 125.94;
+            points['from4'] = points['to4'];
     }
     return points;
 }
