39801261d1b33e4a7f8e2061be5cd140977c7955
achieti <a.chieti@aress.regione.puglia.it>
2024-11-28 08:32:47 +0100

    Update cronicita.js
    Aggiunta una matrice di valori per il grafico Trend di Prevalenza della "PATOLOGIA" per Area Geografica che serve per mostrare una linea rossa continua

--- cronicita.js
+++ cronicita.js
@@ -22,3 +22,26 @@ var yearSelectedForFilter = "2022";
 
 var minYearStarting = 1;
 var maxYearStopping= 17;
+
+// è la linea rossa del grafico Trend di Prevalenza del XXXX per Area Geografica
+// quando è selezionato il "Rischio Relativo"
+var geoAreaRedLineMatrix = [
+    [1, 1, 1], //pre 2006
+    [1, 1, 1], //2006
+    [1, 1, 1], //2007
+    [1, 1, 1], //2008
+    [1, 1, 1], //2009
+    [1, 1, 1], //2010
+    [1, 1, 1], //2011
+    [1, 1, 1], //2012
+    [1, 1, 1], //2013
+    [1, 1, 1], //2014
+    [1, 1, 1], //2015
+    [1, 1, 1], //2016
+    [1, 1, 1], //2017
+    [1, 1, 1], //2018
+    [1, 1, 1], //2019
+    [1, 1, 1], //2020
+    [1, 1, 1], //2021
+    [1, 1, 1], //2022
+];
