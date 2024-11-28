fdfb9d5cfdcfe73c9c7edb2e0a7d47bef25decf2
achieti <a.chieti@aress.regione.puglia.it>
2024-11-28 10:00:16 +0100

    Update cronicita.js
    per generalizzare il codice del grafici, in modo tale da poter fare copia e incolla nel caso di codice non generalizzabile e dato che la differenza per le cronicità sta nella parte testuale e in particolare nell'articolo, ho creato questa funzione pathologyArticle che restituisce l'articolo corretto a seconda della patologia in questione

--- cronicita.js
+++ cronicita.js
@@ -1,27 +1,77 @@
-var yearOptions = [
-  { val: "2022", checked: true },
-  { val: "2021", checked: false },
-  { val: "2020", checked: false },
-  { val: "2019", checked: false },
-  { val: "2018", checked: false },
-  { val: "2017", checked: false },
-  { val: "2016", checked: false },
-  { val: "2015", checked: false },
-  { val: "2014", checked: false },
-  { val: "2013", checked: false },
-  { val: "2012", checked: false },
-  { val: "2011", checked: false },
-  { val: "2010", checked: false },
-  { val: "2009", checked: false },
-  { val: "2008", checked: false },
-  { val: "2007", checked: false },
-  { val: "2006", checked: false },
+var yearOptions = [{
+        val: "2022",
+        checked: true
+    },
+    {
+        val: "2021",
+        checked: false
+    },
+    {
+        val: "2020",
+        checked: false
+    },
+    {
+        val: "2019",
+        checked: false
+    },
+    {
+        val: "2018",
+        checked: false
+    },
+    {
+        val: "2017",
+        checked: false
+    },
+    {
+        val: "2016",
+        checked: false
+    },
+    {
+        val: "2015",
+        checked: false
+    },
+    {
+        val: "2014",
+        checked: false
+    },
+    {
+        val: "2013",
+        checked: false
+    },
+    {
+        val: "2012",
+        checked: false
+    },
+    {
+        val: "2011",
+        checked: false
+    },
+    {
+        val: "2010",
+        checked: false
+    },
+    {
+        val: "2009",
+        checked: false
+    },
+    {
+        val: "2008",
+        checked: false
+    },
+    {
+        val: "2007",
+        checked: false
+    },
+    {
+        val: "2006",
+        checked: false
+    },
 ];
 
 var yearSelectedForFilter = "2022";
 
 var minYearStarting = 1;
-var maxYearStopping= 17;
+var maxYearStopping = 17;
 
 // è la linea rossa del grafico Trend di Prevalenza del XXXX per Area Geografica
 // quando è selezionato il "Rischio Relativo"
@@ -44,5 +94,24 @@ var geoAreaRedLineMatrix = [
     [1, 1, 1], //2020
     [1, 1, 1], //2021
     [1, 1, 1], //2022
-    [1, 1, 1] //2023
+    [1, 1, 1], //2023
 ];
+
+// in base alla patologia restituisce l'articolo determinativo corretto
+function pathologyArticle(pathology) {
+    var article = '';
+    switch (pathology) {
+        case 'Scompenso Cardiaco':
+            article = ' dello ';
+            break;
+        case 'BPCO':
+            article = 'della ';
+            break;
+        case 'Ipertensione':
+            article = " dell' ";
+            break;
+        default:
+            article = ' del ';
+    }
+    return article;
+}
