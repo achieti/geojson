57d10a5ea51b9bd331afa80513af58b91b1df58c
achieti <a.chieti@aress.regione.puglia.it>
2024-11-28 08:58:13 +0100

    Update cronicita-grafico5.js

--- cronicita-grafico5.js
+++ cronicita-grafico5.js
@@ -36,44 +36,6 @@ function standardRate(dataNew, sex = "Maschi", k = 1000) {
     return rates;
 }
 
-// questa funzione calcola lo standard rate solo dell'ultima colonna del grafico,
-// per cui la parte totale (in base ai filtri selezionati)
-function standardRate(dataNew, sex = "Maschi", k = 1000) {
-    let dataset = null;
-    let wi = calculationWi(dataNew, sex);
-
-    let ti = 0;
-    let summation = {
-        numerator: 0,
-        denominator: 0,
-    };
-    let rates = 0;
-
-    let cases = 0;
-    let population = 0;
-    dataset = dataNew.filter((d) => d.sesso === sex);
-
-    classes = dataset.reduce((l, i) => (l.indexOf(i.classe_eta) !== -1 ? l : l.concat([i.classe_eta])), []);
-    classes.sort();
-    classes.forEach((classe) => {
-        let cases = 0;
-        let population = 0;
-        dataset.forEach((row) => {
-            if (row.classe_eta === classe) {
-                cases += row.casi;
-                population += row.popolazione;
-            }
-        });
-        ti = population !== 0 ? cases / population : 0;
-        summation.numerator += wi[classe] * ti;
-        summation.denominator += wi[classe];
-    });
-
-    rates = summation.numerator / summation.denominator;
-    rates = k !== 1000 ? (rates = rates * k) : (rates = +(rates * k)); //.toFixed(2)
-    return rates;
-}
-
 function calculationWi(dataNew, sex = "Maschi") {
     let container = extractTotal(dataNew, "peso_classe", sex);
     let dataset = [];
@@ -83,4 +45,22 @@ function calculationWi(dataNew, sex = "Maschi") {
         obj[row.classe_eta] = row.peso_classe / container;
     });
     return obj;
+}
+
+function extractTotal(dataNew, column, sex = "Maschi") {
+    let dataset = null;
+    let dataRif = null;
+    let sum = 0;
+    const references = dataNew.reduce(
+        (l, i) => (l.indexOf(i.riferimento) !== -1 ? l : l.concat([i.riferimento])),
+        []
+    );
+    if (references.length > 1 && column === "peso_classe") {
+        dataRif = dataNew.filter((d) => d.riferimento === dataNew[0].riferimento);
+    } else {
+        dataRif = dataNew;
+    }
+    dataset = dataRif.filter((d) => d.sesso === sex);
+    dataset.forEach((row) => (sum += row[column]));
+    return sum;
 }
